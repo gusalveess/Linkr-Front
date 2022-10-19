54735996e8274f7946e7b4594fddaa2a110e9315
Gustavo <gustaalveess1302@gmail.com>
2022-10-19 20:56:22 -0300

    building

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,25 @@
 import axios from "axios";
 import styled from "styled-components";
+import { AiOutlineLeft } from 'react-icons/ai'
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function Header() {
+
   return (
     <>
       <Top>
         <h1>Linkr</h1>
+
+        <div></div>
+
+        <div>
+          <AiOutlineLeft />
+        </div>
       </Top>
+      <Hidden>
+        <div>Logout</div>
+      </Hidden>
     </>
   );
 }
@@ -27,4 +38,10 @@ const Top = styled.div`
     color: #fff;
     font-size: 49px;
   }
+
+  svg {
+    color: #fff;
+  }
 `;
+
+const Hidden = styled
