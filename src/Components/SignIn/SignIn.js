6b0cfd1f02895a4d3c7bbbcb9853bc71311c12de
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2022-10-23 17:41:58 -0300

    feat: responsive layout

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -20,7 +20,7 @@ export default function SignIn() {
 	 	if (isLogged) {
 	  		Navigate("/timeline");
 	  	};
-	  });
+	});
 
 	function Post() {
 		const body = {
@@ -61,49 +61,47 @@ export default function SignIn() {
 	}
 
 	return (
-		<>
-			<Body>
-				<MainOne>
-					<div>
-						<h1>Linkr</h1>
-						<h2>save, share and discover the best links on the web</h2>
-					</div>
-				</MainOne>
+		<Body>
+			<MainOne>
+				<div>
+					<h1>linkr</h1>
+					<h2>save, share and discover the best links on the web</h2>
+				</div>
+			</MainOne>
 
-				<MainTwo>
-					<form onSubmit={HandleForm}>
-						<input
-							type="email"
-							placeholder="e-mail"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							disabled={disable}
-							required
-						/>
-						<input
-							type="password"
-							placeholder="senha"
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-							disabled={disable}
-							required
-						/>
+			<MainTwo>
+				<form onSubmit={HandleForm}>
+					<input
+						type="email"
+						placeholder="e-mail"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						disabled={disable}
+						required
+					/>
+					<input
+						type="password"
+						placeholder="senha"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						disabled={disable}
+						required
+					/>
 
-						<button disabled={disable}>
-							{disable ? (
-								<ThreeDots color="#FFFFFF" height={13} width={51} />
-							) : (
-								"Log In"
-							)}
-						</button>
-					</form>
+					<button disabled={disable}>
+						{disable ? (
+							<ThreeDots color="#FFFFFF" height={13} width={51} />
+						) : (
+							"Log In"
+						)}
+					</button>
+				</form>
 
-					<p onClick={() => Navigate("/sign-up")}>
-						First time? Create an account!
-					</p>
-				</MainTwo>
-			</Body>
-		</>
+				<p onClick={() => Navigate("/sign-up")}>
+					First time? Create an account!
+				</p>
+			</MainTwo>
+		</Body>
 	);
 }
 
@@ -113,6 +111,11 @@ const Body = styled.div`
 	height: 100vh;
 	display: flex;
 	overflow-y: scroll;
+
+
+	@media(max-width: 740px) {
+		flex-direction: column;
+	}
 `;
 
 const MainOne = styled.div`
@@ -144,6 +147,25 @@ const MainOne = styled.div`
 		color: #fff;
 		font-size: 43px;
 	}
+
+	@media(max-width: 740px) {
+		width: 100%;
+    	height: fit-content;
+		text-align: center;
+
+		div {
+			margin: 30px auto;
+    		width: 80%;
+		}
+
+		h1 {
+			font-size: 76px;
+		}
+
+		h2 {
+			font-size: 23px;
+		}
+	}
 `;
 
 const MainTwo = styled.div`
@@ -156,6 +178,8 @@ const MainTwo = styled.div`
 	flex-direction: column;
 
 	form {
+		width: 90%;
+		max-width: 429px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
@@ -163,7 +187,7 @@ const MainTwo = styled.div`
 	}
 
 	input {
-		width: 429px;
+		width: 100%;
 		height: 65px;
 		margin-bottom: 13px;
 		border: none;
@@ -184,7 +208,7 @@ const MainTwo = styled.div`
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		width: 442px;
+		width: 100%;
 		height: 65px;
 		margin-bottom: 22px;
 		border: none;
@@ -212,5 +236,33 @@ const MainTwo = styled.div`
 		color: #fff;
 		text-decoration: underline white;
 		cursor: pointer;
+	}
+
+	@media(max-width: 740px) {
+		width: 100%;
+		height: auto;
+		height: 100%;
+		justify-content: flex-start;
+
+		form {
+			margin: 40px auto 0;
+		}
+
+		p {
+			margin-bottom: 40px;
+			font-size: 17px;
+		}
+
+		input {
+			font-size: 22px;
+		}
+
+		input::placeholder {
+			font-size: 22px;
+		}
+
+		button {
+			font-size: 22px;
+		}
 	}
 `;
